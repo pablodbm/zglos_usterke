32ffab24aca666a5bfcb2004daa84e4140ac46b4
Kacper <przelozny14@gmail.com>
2026-05-09 13:45:03 +0200

    projekt

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'zglos-usterke';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
